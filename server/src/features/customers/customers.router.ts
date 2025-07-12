61a4618b64cfd2ea4aba7763a3e08914ff56c854
Inbal Gordon <inbalgordon@gmail.com>
2025-07-12 19:51:13 +0200

    feat: get api for customerRouter

--- server/src/features/customers/customers.router.ts
+++ server/src/features/customers/customers.router.ts
@@ -1,3 +1,22 @@
 import express from "express";
+import { getCustomerDetail, getCustomers } from "./customers.service";
 
 export const customersRouter = express.Router();
+
+// get list
+customersRouter.get("/" , async (req, res) => {
+  const customers = await getCustomers()
+  res.json(customers)
+})
+
+// get details
+
+customersRouter.get("/:id", async (req, res) => {
+  const id = req.params.id
+  const customer = await getCustomerDetail(id)
+  if (customer != null) {
+    res.json(customer)
+  } else {
+    res.status(404).json({ message: "Customer Not Found"})
+  }
+});
